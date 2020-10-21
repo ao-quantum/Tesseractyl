ab76d09192beb7e393c86a63344ecd8df541204f
Quantum <killinggamer4mc@gmail.com>
2020-10-22 00:23:57 +0100

    stuff

--- tests/getServers.test.js
+++ tests/getServers.test.js
@@ -1,7 +1,8 @@
+const { default: Collection } = require('@discordjs/collection');
 let index = require('../lib/index');
 let config = require('./config.json')
 
 test('get all servers', () => {
     let Client = new index.default.Client(config.apikey, config.panelURL)
-    expect(Client.getServers()).resolves.toBeInstanceOf(Array)
+    expect(Client.getServers()).resolves.toBeInstanceOf(Collection)
 })
